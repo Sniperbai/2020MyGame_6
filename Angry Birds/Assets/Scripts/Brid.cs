3207df2deb5c35cbc0da205b3c5ae545d3fcd3a9
白滨 <306558437@qq.com>
2020-07-23 07:55:31 +0800

    05-拖拽距离最大限定

--- Angry Birds/Assets/Scripts/Brid.cs
+++ Angry Birds/Assets/Scripts/Brid.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Brid : MonoBehaviour
 {
     private bool isclick = false ;
+    public Transform rightPos;
+    public float maxDis = 3;
+
     private void OnMouseDown()
     {
         isclick = true;
@@ -21,6 +24,13 @@ public class Brid : MonoBehaviour
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             //transform.position += new Vector3(0, 0, 10);
             transform.position += new Vector3(0, 0, -Camera.main.transform.position.z);
+
+            if (Vector3.Distance(transform.position, rightPos.position) > maxDis) //进行位置限定
+            {
+                Vector3 pos = (transform.position - rightPos.position).normalized;//单位华向量
+                pos *= maxDis;//最大长度的向量
+                transform.position = pos + rightPos.position;
+            }
         }
     }
 }
