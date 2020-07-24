27b7d2b3d4d5e0dd6ee26b0e0eb4f571ce005306
白滨 <306558437@qq.com>
2020-07-24 08:06:52 +0800

    06-小鸟的飞出

--- Angry Birds/Assets/Scripts/Brid.cs
+++ Angry Birds/Assets/Scripts/Brid.cs
@@ -7,15 +7,26 @@ public class Brid : MonoBehaviour
     private bool isclick = false ;
     public Transform rightPos;
     public float maxDis = 3;
+    private SpringJoint2D sp;
+    private Rigidbody2D rg;
+
+    private void Awake()
+    {
+        sp = GetComponent<SpringJoint2D>();
+        rg = GetComponent<Rigidbody2D>();
+    }
 
     private void OnMouseDown()
     {
         isclick = true;
+        rg.isKinematic = true;
     }
 
     private void OnMouseUp()
     {
         isclick = false;
+        rg.isKinematic = false;
+        Invoke("Fly", 0.1f);
     }
 
     private void Update()
@@ -32,5 +43,10 @@ public class Brid : MonoBehaviour
                 transform.position = pos + rightPos.position;
             }
         }
+    }
+
+    void Fly() 
+    {
+        sp.enabled = false;
     }
 }
