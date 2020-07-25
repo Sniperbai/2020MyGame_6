663b8a1e1465c03be57707c7fc3aee292ed3118b
白滨 <306558437@qq.com>
2020-07-25 12:34:51 +0800

    07-猪的受伤

---
+++ Angry Birds/Assets/Scripts/Pig.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pig : MonoBehaviour
+{
+    public float maxSpeed = 10;
+    public float misSpeed = 5;
+
+    private SpriteRenderer render;
+    public Sprite hurt;
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude > maxSpeed)//直接死亡
+        {
+            Destroy(gameObject);
+        }
+        else if(collision.relativeVelocity.magnitude > misSpeed && collision.relativeVelocity.magnitude < maxSpeed)
+        {
+            render.sprite = hurt;
+        }
+    }
+}
