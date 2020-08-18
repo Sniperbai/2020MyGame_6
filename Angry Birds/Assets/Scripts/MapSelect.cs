5940e8d71416af23a8e4f3fad6d8f599bba07010
白滨 <306558437@qq.com>
2020-08-18 09:19:31 +0800

    地图选择逻辑

---
+++ Angry Birds/Assets/Scripts/MapSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelect : MonoBehaviour
+{
+    public int starsNum = 0;
+    private bool isSelect = false;
+
+    public GameObject locks;
+    public GameObject stars;
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetInt("totalNum", 0) >= starsNum)
+        {
+            isSelect = true;
+        }
+
+        if (isSelect)
+        {
+            locks.SetActive(false);
+            stars.SetActive(true);
+        }
+    }
+}
